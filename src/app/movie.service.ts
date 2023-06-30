00af4b3b3acdab4f64371eed6cdcf05e69644f1b
Washington Ferreira <35137108+Batcleta@users.noreply.github.com>
2023-06-30 09:38:26 -0300

    search api function

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -7,12 +7,12 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class MovieService {
-  private readonly apiUrl = 'http://localhost:3004/movies';
+  private readonly dbUrl = 'http://localhost:3004/movies';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getMoviesList(): Observable<movieObject[]> {
-    return this.http.get<movieObject[]>(this.apiUrl);
+    return this.http.get<movieObject[]>(this.dbUrl);
   }
 
   fetchMovies(): Promise<movieObject[]> {
@@ -29,8 +29,18 @@ export class MovieService {
   }
 
   updateMovie(movie: movieObject): Observable<movieObject> {
-    const url = `${this.apiUrl}/${movie.id}`;
+    const url = `${this.dbUrl}/${movie.id}`;
     return this.http.put<movieObject>(url, movie);
   }
 
+  searchMoviesFromApi(movieName: string): Observable<movieObject[]> {
+    const url = `https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=1`;
+    return this.http.get<movieObject[]>(url, {
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjkwZTdiN2ZlMjcxMmQ5OTY5NWM0NTczMWY2ZGI3MiIsInN1YiI6IjY0OWMyNDEyZmQ0ZjgwMDEwZDk4YWI0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pyZGT1Z8mvq_BZ_Zu8zqmAehmQq4VPvuQxn5LLSacbU',
+      },
+    });
+  }
 }
